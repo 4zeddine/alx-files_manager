b562bdf89b4191d1a3158fbe46ad5904a3334feb
4zeddine <azeddine.nader@gmail.com>
2024-10-02 23:27:06 +0100

    tasks commit

--- utils/redis.js
+++ utils/redis.js
@@ -2,58 +2,58 @@ import { promisify } from 'util';
 import { createClient } from 'redis';
 
 /**
- * Represents a client for Redis database.
+ * Represents a Redis client.
  */
 class RedisClient {
   /**
-     * Initializes a new instance of RedisClient.
-     */
+   * Creates a new RedisClient instance.
+   */
   constructor() {
     this.client = createClient();
-    this.isConnected = true;
-    this.client.on('error', (error) => {
-      console.error('Failed to connect to Redis client:', error.message || error.toString());
-      this.isConnected = false;
+    this.isClientConnected = true;
+    this.client.on('error', (err) => {
+      console.error('Redis client failed to connect:', err.message || err.toString());
+      this.isClientConnected = false;
     });
     this.client.on('connect', () => {
-      this.isConnected = true;
+      this.isClientConnected = true;
     });
   }
 
   /**
-     * Checks if the connection to the Redis server is active.
-     * @returns {boolean}
-     */
+   * Checks if this client's connection to the Redis server is active.
+   * @returns {boolean}
+   */
   isAlive() {
-    return this.isConnected;
+    return this.isClientConnected;
   }
 
   /**
-     * Fetches the value associated with a specified key.
-     * @param {String} key The key of the item to fetch.
-     * @returns {String | Object}
-     */
+   * Retrieves the value of a given key.
+   * @param {String} key The key of the item to retrieve.
+   * @returns {String | Object}
+   */
   async get(key) {
     return promisify(this.client.GET).bind(this.client)(key);
   }
 
   /**
-     * Saves a key-value pair with an expiration time.
-     * @param {String} key The key of the item to save.
-     * @param {String | Number | Boolean} value The value to save.
-     * @param {Number} duration The expiration duration in seconds.
-     * @returns {Promise<void>}
-     */
+   * Stores a key and its value along with an expiration time.
+   * @param {String} key The key of the item to store.
+   * @param {String | Number | Boolean} value The item to store.
+   * @param {Number} duration The expiration time of the item in seconds.
+   * @returns {Promise<void>}
+   */
   async set(key, value, duration) {
     await promisify(this.client.SETEX)
       .bind(this.client)(key, duration, value);
   }
 
   /**
-     * Deletes the value associated with a specified key.
-     * @param {String} key The key of the item to delete.
-     * @returns {Promise<void>}
-     */
+   * Removes the value of a given key.
+   * @param {String} key The key of the item to remove.
+   * @returns {Promise<void>}
+   */
   async del(key) {
     await promisify(this.client.DEL).bind(this.client)(key);
   }
